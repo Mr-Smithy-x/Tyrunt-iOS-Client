8b9330c135b0150fddb3cf06bb975c7a453f415e
Charlton Smith <CharltonSmith@outlook.com>
2020-09-23 22:04:33 -0400

    Initial Commit

---
+++ TyruntApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TyruntApp
+//
+//  Created by Charlton Smith on 9/23/20.
+//  Copyright © 2020 Charlton Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
